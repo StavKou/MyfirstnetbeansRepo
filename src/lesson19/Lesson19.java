43e0f8d2155b92e35bf0277ebc589a930553ed23
StavKou <stkourkoulos@gmail.com>
2022-05-04 20:01:02 +0300

    add a second System.out.println("another hello1");

--- src/lesson19/Lesson19.java
+++ src/lesson19/Lesson19.java
@@ -17,6 +17,7 @@ public class Lesson19 {
     public static void main(String[] args) {
         
         System.out.println("another hello");
+        System.out.println("another hello1");
     }
     
 }
